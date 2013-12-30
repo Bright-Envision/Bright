5102b8364556f4986c648067d0f40447bb100fc5
Ryan Clough <ryanc1256@gmail.com>
2013-12-30 22:26:13 +1300

    Added the rudimentary functions for the base of the framework.

--- base.js
+++ base.js
@@ -1,4 +1,7 @@
 (function(window, undefined){
+	var w3c = !!window.addEventListener,
+		class2type = {};
+
 	var bright = function(selector, context){
 	 	return new bright.fn.init(selector, context);	
 	}
@@ -11,7 +14,69 @@
 		}
 	}
 
-	bright.prototype = bright.fn;
+	bright.fn.init.prototype = bright.fn;
+
+
+
+	bright.extend = bright.fn.extend = function(dest, src, clone, deep){
+		var i = 1, deep, length;
+
+		if (!src || !dest) {
+			return null;
+		}
+
+		if (typeof dest == 'boolean'){
+			deep = dest;
+			i = 2;
+		}
+
+		if (typeof dest == 'object' && !bright.isFunction(dest)){
+			dest = {};
+		}
+
+		if (length === i){
+			dest = this;
+			i--;
+		}
+
+		for (;i < length; i++){
+
+		}
+
+
+		if (isArray(src) || isArray(dest)){
+			alert('test');
+		}
+
+	}
+
+
+	bright.each = bright.fn.each = function(){
+		
+	}
+
+	bright.fn.isArray = Array.isArray || function( obj ) {
+		return bright.type( obj ) === 'array';
+	}
+
+	bright.fn.isFunction = function( obj ){
+		return bright.type( obj ) === 'function';
+	}
+
+
+	bright.fn.type = function( obj ) {
+		if (obj == null){
+			return String(obj);
+		}
+
+		return typeof obj === "object" || typeof obj === "function" ?
+			class2type[ core_toString.call(obj) ] || "object" :
+			typeof obj;
+	}
+
+
+
+
 
 	window.bright = bright;
 
