fee63e51844c99d0d595ebcfb6227e35a2b893f6
Ryan Clough <ryanc1256@gmail.com>
2014-01-01 09:13:28 +1300

    Just added some standard events. click, hover, mouse leave, mouse enter

--- src/core/core.js
+++ src/core/core.js
@@ -205,10 +205,11 @@
 	}
 	
 
+	//standard events
+
 	bright.extend(bright.fn, {
 		click: function(callback){
 			if (w3c){
-				console.log(this);
 				if (this.elements) {
 					bright.each(this.elements, function(i, val){
 						if (val){
@@ -217,11 +218,81 @@
 					});
 				}
 			} else {
-
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.attachEvent('onclick', callback);
+						}
+					});
+				}
 			}
+			return this;
+		},
+		hover: function(fnOver, fnOut){
+			return this.mouseenter(fnOver).mouseleave(fnOut || fnOver);
+		},
+		mouseenter: function(callback){
+			if (w3c){
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.addEventListener('mouseenter', callback, false);
+						}
+					});
+				}
+			} else {
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.attachEvent('onmouseenter', callback);
+						}
+					});
+				}
+			}
+			return this;
+		}, 
+		mouseleave: function(callback){
+			if (w3c){
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.addEventListener('mouseleave', callback, false);
+						}
+					});
+				}
+			} else {
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.attachEvent('onmouseleave', callback);
+						}
+					});
+				}
+			}
+			return this;
+		},
+		mousemove: function(callback){
+			if (w3c){
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.addEventListener('mousemove', callback, false);
+						}
+					});
+				}
+			} else {
+				if (this.elements) {
+					bright.each(this.elements, function(i, val){
+						if (val){
+							val.attachEvent('onmousemove', callback);
+						}
+					});
+				}
+			}
+			return this;
 		}
-
 	});
+
 
 	/* main selector for the new library */
 
